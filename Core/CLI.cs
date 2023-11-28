db0b29d8d295e87f16846e5c410d71bb02b39738
chris1320 <chris1320is@protonmail.com>
2023-11-28 23:02:05 +0800

    refactor: change `CLI.cs` methods into lambdas
    Signed-off-by: chris1320 <chris1320is@protonmail.com>

--- Core/CLI.cs
+++ Core/CLI.cs
@@ -5,23 +5,15 @@ using Spectre.Console;
 /// </summary>
 class CLI
 {
-    public static string Note(string str, bool escape_markup = true)
-    {
-        return $"[gray][[[/][green]i[/][gray]]][/] {(escape_markup ? str.EscapeMarkup() : str)}";
-    }
+    public static string Note(string str, bool escape_markup = true) =>
+        $"[gray][[[/][green]i[/][gray]]][/] {(escape_markup ? str.EscapeMarkup() : str)}";
 
-    public static string Warn(string str, bool escape_markup = true)
-    {
-        return $"[gray][[[/][yellow]![/][gray]]][/] {(escape_markup ? str.EscapeMarkup() : str)}";
-    }
+    public static string Warn(string str, bool escape_markup = true) =>
+        $"[gray][[[/][yellow]![/][gray]]][/] {(escape_markup ? str.EscapeMarkup() : str)}";
 
-    public static string Error(string str, bool escape_markup = true)
-    {
-        return $"[gray][[[/][red]E[/][gray]]][/] {(escape_markup ? str.EscapeMarkup() : str)}";
-    }
+    public static string Error(string str, bool escape_markup = true) =>
+        $"[gray][[[/][red]E[/][gray]]][/] {(escape_markup ? str.EscapeMarkup() : str)}";
 
-    public static string Help(string str, bool escape_markup = true)
-    {
-        return $"[gray][[[/][cyan]?[/][gray]]][/] {(escape_markup ? str.EscapeMarkup() : str)}";
-    }
+    public static string Help(string str, bool escape_markup = true) =>
+        $"[gray][[[/][cyan]?[/][gray]]][/] {(escape_markup ? str.EscapeMarkup() : str)}";
 }
