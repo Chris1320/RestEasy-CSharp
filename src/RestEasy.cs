741971918f5399723b750054b8d872cffb9b179b
Chris1320 <chris1320is@protonmail.com>
2023-12-10 12:35:59 +0800

    refactor: update help menu descriptions
    Signed-off-by: Chris1320 <chris1320is@protonmail.com>

--- src/RestEasy.cs
+++ src/RestEasy.cs
@@ -18,43 +18,45 @@ class RestEasy
 
             config
                 .AddCommand<InitCommand>("init")
-                .WithDescription($"Initialize a new {Info.Name} vault.")
+                .WithDescription($"Initialize a new {Info.Name} vault")
                 .WithExample(
                     new string[] { "init", "--vault", "~/backups/desktop1/resteasy/vault" }
                 );
             config
                 .AddCommand<AddCommand>("add")
                 .WithDescription(
-                    $"Add a new restic repository within an existing {Info.Name} vault."
+                    $"Add a new restic repository within an existing {Info.Name} vault"
                 )
                 .WithExample(
                     new string[] { "add", "--name", "test-repo", "foo.txt", "bar.png", "baz/" }
                 );
             config
                 .AddCommand<ListCommand>("list")
-                .WithDescription($"List restic repositories in a {Info.Name} vault.")
+                .WithDescription($"List restic repositories in a {Info.Name} vault")
                 .WithExample(
                     new string[] { "list", "--vault", "~/backups/desktop1/resteasy/vault" }
                 );
             config
                 .AddCommand<InfoCommand>("info")
-                .WithDescription("Get information about a restic repository.")
+                .WithDescription(
+                    $"Get information about a {Info.Name} vault or a restic repository"
+                )
                 .WithExample(new string[] { "info", "repo-A" });
             config
                 .AddCommand<BackupCommand>("backup")
                 .WithDescription(
-                    "Perform a backup of a single, a group of, or all restic repositories."
+                    "Perform a backup of a single, a group of, or all restic repositories"
                 )
                 .WithExample(new string[] { "backup", "repo-A", "repo-B", "repo-C" });
             config
                 .AddCommand<RestoreCommand>("restore")
                 .WithDescription(
-                    "Restore a backup of a single, a group of, or all restic repositories."
+                    "Restore a backup of a single, a group of, or all restic repositories"
                 )
                 .WithExample(new string[] { "restore", "repo-A", "repo-B", "repo-C" });
             config
                 .AddCommand<RemoveCommand>("remove")
-                .WithDescription("Remove one or more restic repositories.")
+                .WithDescription("Remove one or more restic repositories")
                 .WithExample(new string[] { "remove", "repo-A", "repo-B", "group-A" });
         });
 
